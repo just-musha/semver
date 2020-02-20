2920361ada6f2e754e8bb8d4cd104ee0329ea0e7
Maria Bulatova <maria.r.bulatova@yandex.ru>
2020-02-20 15:51:44 +0300

    Initail commit

---
+++ README.md
@@ -0,0 +1,32 @@
+semver
+========
+
+CLI tool for git semantic versioning
+
+## Usage
+
+TODO
+
+Example:
+
+```
+$ TODO
+```
+
+## Installation From Source
+
+To install the package after you've cloned the repository, you'll want to run the following command from within the project directory:
+
+```
+$ pip install --user -e .
+```
+
+## Preparing for Development
+
+Follow these steps to start developing with this project:
+
+1. Ensure `pip` and `pipenv` are installed
+2. Clone repository: `git clone git@github.com:example/semver`
+3. `cd` into the repository
+4. Activate virtualenv: `pipenv shell`
+5. Install dependencies: `pipenv install`
---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import find_packages, setup
+
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+setup(
+    name='semver',
+    version='0.1.0',
+    author='mbulatova',
+    author_email='maria.r.bulatova@yandex.ru',
+    description='Git semantic versioning tool',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/mbulatova/semver',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    install_requires=[],
+    python_requires='>=3.6',
+    entry_points={
+        'console_scripts':[
+            'semver=semver.cli:main'
+        ],
+    }
+)
